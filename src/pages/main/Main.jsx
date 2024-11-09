99d6d03629c1d423c421ca3177e951caa0f3e360
yuni <lyhhh0320@gmail.com>
2024-11-10 04:07:35 +0900

    fix: main style

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -65,9 +65,8 @@ const MainWrapper = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
-  margin-top: -40px;
   flex-direction: row;
   justify-content: center;
-  align-items: center;
+  margin-top: 6.5%;
   gap: 87px;
 `;
